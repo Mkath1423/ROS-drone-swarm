d0ea4b057f3128a9468650a5a004aaa8147d42bd
Lex Stapleton <diamondcrafter1423@gmail.com>
2021-07-19 13:54:29 -0400

    Added Multiple Controls
    Edited launch file to create multiple drones
    
    Edited drone_controller to support multiple instances
    
    Made Ground Control GUI app to send control to individual drones

--- scripts/drone_controller.py
+++ scripts/drone_controller.py
@@ -3,16 +3,18 @@
 import rospy
 import sys
 import numpy as np
+import tf
  
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
-#from drone_swarm.srv import SetPID
- 
+from drone_swarm.srv import *
+from drone_swarm.msg import *
+
 # PID constants and variables
 
-P_GAIN = 2
-I_GAIN = 0.1
-D_GAIN = 7
+P_GAIN = 5
+I_GAIN = 0
+D_GAIN = 5
  
 current_pose = [0, 0, 0, 0, 0, 0]
 
@@ -25,53 +27,23 @@ target_state = [0, 0, 10, 0, 0, 0]
 
 PID = lambda e, el, et, max_accel : max( min(P_GAIN * e + I_GAIN * et + D_GAIN * (e - el), max_accel), -1 * max_accel)
  
- # quaternion_rotation_matrix function by Addison Sers-Collins 
- # https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/
-def quaternion_rotation_matrix(Q):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
  
-    Input
-    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
- 
-    Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
-             This rotation matrix converts a point in the local reference 
-             frame to a point in the global reference frame.
-    """
-    # Extract the values from Q
-    q0 = Q[0]
-    q1 = Q[1]
-    q2 = Q[2]
-    q3 = Q[3]
-     
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-     
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-     
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-     
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-                            
-    return rot_matrix
+def set_pid(data):
+	global P_GAIN
+	global I_GAIN
+	global D_GAIN
 
-def set_pid(req):
-	P_GAIN = req.p
-	I_GAIN = req.i
-	D_GAIN = req.d
-
+	P_GAIN = data.p
+	I_GAIN = data.i
+	D_GAIN = data.d
+	
+	print(f"{P_GAIN} {I_GAIN} {D_GAIN}")
+	
+def set_target(data):
+	global target_state
+	
+	target_state = [data.x_pos, data.y_pos, data.z_pos, data.x_ang, data.y_ang, data.z_ang]
+	print(target_state)
 
 def ParseArgs(args, out):
 	for arg in args:
@@ -88,7 +60,7 @@ ParseArgs(sys.argv, arguments)
 if(any([not i in arguments.keys() for i in mandatory_keys])):
 	raise ValueError('Not all mandatory keys are present. Check node arguments')
 
-rospy.init_node("drone_controller")
+rospy.init_node(f"drone_{arguments['model']}")
 print(f'drone controller for model {arguments["model"]} has started')
 
 # Publisher Setup
@@ -101,27 +73,31 @@ get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 model = GetModelStateRequest()
 model.model_name = arguments['model']
 
-#rospy.init_node('set_pid')
-#s = rospy.Service('set_pid', SetPID, set_pid)
-#rospy.spin()
+set_pid_service = rospy.Subscriber('set_pid', SetPID, set_pid)
+set_target_sub = rospy.Subscriber('set_target', SetTarget, set_target)
 
 
 print(f'drone controller for model {arguments["model"]} has made connection to service')
 
 # Main loop
-r = rospy.Rate(20)
+r = rospy.Rate(100)
 
 while not rospy.is_shutdown():
 	last_error = current_error.copy()
 	
 	current_state = get_model_srv(model)
 	
+	current_rotation = tf.transformations.euler_from_quaternion([current_state.pose.orientation.w,
+						      current_state.pose.orientation.x,
+						      current_state.pose.orientation.y,
+						      current_state.pose.orientation.z])
+	
 	current_pose = [current_state.pose.position.x,
 			current_state.pose.position.y,
 			current_state.pose.position.z,
-			current_state.pose.orientation.x,
-			current_state.pose.orientation.y,
-			current_state.pose.orientation.z]
+			current_rotation[0],
+			current_rotation[1],
+			current_rotation[2]]
 	
 	# calculate error
 	current_error = [target_state[i] - current_pose[i] for i in range(6)]
@@ -138,26 +114,26 @@ while not rospy.is_shutdown():
 	# pose
 	new_state.pose = current_state.pose
 	
-	rot_matricx = quaternion_rotation_matrix([current_state.pose.orientation.w,
-						   current_state.pose.orientation.x,
-						   current_state.pose.orientation.y,
-						   current_state.pose.orientation.z])
+	#rot_matricx = quaternion_rotation_matrix([current_state.pose.orientation.w,
+	#					   current_state.pose.orientation.x,
+	#					   current_state.pose.orientation.y,
+	#					   current_state.pose.orientation.z])
 						   
 	#print(f'{current_state.pose.orientation} -> {rot_matricx}')
 	
-	acceleration = np.array([PID(current_error[0], last_error[0], total_error[0], 10),
-				 PID(current_error[1], last_error[1], total_error[1], 10),
-				 PID(current_error[2], last_error[2], total_error[2], 10)])
-	
-	acceleration = np.dot(rot_matricx, acceleration)
+	#acceleration = np.array([PID(current_error[0], last_error[0], total_error[0], 10),
+	#			 PID(current_error[1], last_error[1], total_error[1], 10),
+	#			 PID(current_error[2], last_error[2], total_error[2], 10)])
+	#
+	#acceleration = np.dot(rot_matricx, acceleration)
 	
 	# twist
 	new_state.twist.linear.x += PID(current_error[0], last_error[0], total_error[0], 10)
 	new_state.twist.linear.y += PID(current_error[1], last_error[1], total_error[1], 10)
 	new_state.twist.linear.z += PID(current_error[2], last_error[2], total_error[2], 10)
-	new_state.twist.angular.x += PID(current_error[3], last_error[3], total_error[3], 0.1)
-	new_state.twist.angular.y += PID(current_error[4], last_error[4], total_error[4], 0.1)
-	new_state.twist.angular.z += PID(current_error[5], last_error[5], total_error[5], 0.1)
+	# new_state.twist.angular.x += PID(current_error[3], last_error[3], total_error[3], 0.1)
+	# new_state.twist.angular.y += PID(current_error[4], last_error[4], total_error[4], 0.1)
+	#new_state.twist.angular.z += PID(current_error[5], last_error[5], total_error[5], 0.1)
 	
 	state_pub.publish(new_state)
 
---
+++ scripts/ground_control.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python	
+
+import PySimpleGUI as sg
+import rospy
+import sys
+
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest
+from drone_swarm.srv import *
+from drone_swarm.msg import *
+
+### Globals ###
+
+window_size = (1000, 500)
+
+### Parse arguments into a dict ###
+
+def ParseArgs(args, out):
+	for arg in args:
+		if ':=' in arg:
+			temp = arg.split(':=')
+			out[temp[0]] = temp[1]
+			
+
+#arguments = {}
+#ParseArgs(sys.argv, arguments)
+
+#mandatory_keys = ['amount_of_drones']
+
+# Make sure all the nessessary keys are added
+#if(any([not i in arguments.keys() for i in mandatory_keys])):
+#	raise ValueError('Not all mandatory keys are present. Check node arguments')
+
+
+
+### Setup node and ros functionalities ### 
+#rospy.init_node('ground_control')
+
+#rospy.wait_for_service('/gazebo/get_model_state')
+#get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+
+
+#set_pid_publishers = []
+#set_target_publishers = []
+
+#for i in range(1, 1 + int(arguments['amount_of_drones'])):
+#	set_pid_publishers.append(rospy.Publisher(f'/drone{i}/set_pid', SetPID, queue_size=10))
+#	set_target_publishers.append(rospy.Publisher(f'/drone{i}/set_target', SetTarget, queue_size=10))
+
+### Functions ###
+def GetDronePosition(model_name):
+	model = GetModelStateRequest()
+	model.model_name = model_name
+	
+	drone_state = get_model_srv(model)
+	return (drone_state.pose.position.x,
+		drone_state.pose.position.y,
+		drone_state.pose.position.z)
+	
+def WindowSize(x, y):
+    return (round(window_size[0] * x), round(window_size[1] * y))
+    
+# Display a GUI window
+
+
+# Main loop
+#   Control GUI window
+#   Control Drone movemnet by setting targets
+
+sg.theme('Dark')
+manual_control_elements = [[
+			    sg.Frame("Drone Select", [
+			    [sg.Listbox([f'drone{i+1}' for i in range(2)], key='drone_select', size =(15, 19), enable_events=True)]
+			    			      ]),
+			    sg.Column([
+			    [sg.Text(' PID Gains:', size=(12, 1)), 
+			     sg.Text('P:'),sg.Input(key='p_gain', size=(4, 1), enable_events=True),
+			     sg.Text('I:'),sg.Input(key='i_gain', size=(4, 1), enable_events=True),
+			     sg.Text('D:'),sg.Input(key='d_gain', size=(4, 1), enable_events=True),
+			     sg.Button('Send', key='send_pid_gains')],
+			     
+			    [sg.Text('Target Pos:', size=(12, 1)), 
+			     sg.Text('X:'),sg.Input(key='x_target', size=(4, 1), enable_events=True),
+			     sg.Text('Y:'),sg.Input(key='y_target', size=(4, 1), enable_events=True),
+			     sg.Text('Z:'),sg.Input(key='z_target', size=(4, 1), enable_events=True),
+			     sg.Button('Send', key='send_target_pos')]
+					])
+			    ]]
+manual_control_layout = [[sg.Column(manual_control_elements, size=WindowSize(1, 0.8))]]
+
+tabs = sg.TabGroup([[sg.Tab("Manual Control", manual_control_layout)]])
+
+layout = [[sg.Text("Ground Control", size = (50, 1), font=("Helvetica", 20), justification="center")],
+	  [tabs]]
+
+window = sg.Window("Ground Control", layout, size = window_size)
+
+#r = rospy.Rate(100)
+def main():
+	cont = True
+	#while (not rospy.is_shutdown()) and cont:
+	while cont:
+		event, values = window.read()
+		
+		if(event == sg.WINDOW_CLOSED):
+        		cont = False
+		
+		elif(event == 'drone_select'):
+			print(f'Drone Selected: {values["drone_select"][0]}')
+		
+		#r.sleep()
+		
+	window.close()
+
+
+
+if __name__ == '__main__':
+	main()
+
